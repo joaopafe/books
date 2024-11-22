b8cbb708e17b6d40a18d10eb8b3eb5d241396b59
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2024-11-22 18:42:46 -0300

    add the create method

--- backend/book/BookController.js
+++ backend/book/BookController.js
@@ -5,7 +5,27 @@ class BookController {
     await BookRepository.createTable();
   }
 
-  static async listAll() {
-    await BookRepository.listAll();
+  static async listAll(req, res) {
+    const books = await BookRepository.listAll();
+
+    return res.json(books);
+  }
+
+  static async create(req, res) {
+    const title = req.body.title;
+    const author = req.body.author;
+    const publicationDate = req.body.publicationDate;
+    const description = req.body.description;
+    const image = req.body.image;
+
+    await BookRepository.create(
+      title,
+      author,
+      publicationDate,
+      description,
+      image
+    );
+
+    return res.json({ message: "Book created succesfully" });
   }
 }
