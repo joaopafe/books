fb07ada0eaa37a194d5f13f6beda69f0bac1366d
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2024-11-22 18:18:52 -0300

    add the listAll query

--- backend/repository/BookRepository.js
+++ backend/repository/BookRepository.js
@@ -1,16 +1,20 @@
 const openDB = require("../configDB");
 
 class BookRepository {
-  static async createTable() {
-    const db = await openDB();
+  static db = openDB();
 
-    db.exec(
+  static async createTable() {
+    this.db.exec(
       `CREATE TABLE IF NOT EXISTS Book
       (id INTEGER PRIMARY KEY AUTOINCREMENT,
       title TEXT, author TEXT, publication_date DATE,
       description TEXT, image TEXT
       )`
     );
+  }
+
+  static async listAll() {
+    this.db.run(`SELECT * FROM Book`);
   }
 }
 
