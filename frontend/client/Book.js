54ae4cb4628253f6461144025616c384c9ebbde4
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-01-20 16:02:05 -0300

    add the postBook method on the Book client

--- frontend/client/Book.js
+++ frontend/client/Book.js
@@ -2,4 +2,20 @@ class Book {
   static async getBooks() {
     return fetch("http://localhost:3000/book");
   }
+
+  static async postBook(title, author, publicationDate, description, image) {
+    return fetch("http://localhost:3000/book", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        author,
+        publicationDate,
+        description,
+        image,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
