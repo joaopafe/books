62b1dd7509515e6acc034c76e30796aa59f81a8a
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-01-21 09:13:53 -0300

    add the getBookById method on Book client

--- frontend/client/Book.js
+++ frontend/client/Book.js
@@ -3,6 +3,10 @@ class Book {
     return fetch("http://localhost:3000/book");
   }
 
+  static async getBookById(id) {
+    return fetch(`http://localhost:3000/book/${id}`);
+  }
+
   static async postBook(title, author, publicationDate, description, image) {
     return fetch("http://localhost:3000/book", {
       method: "POST",
