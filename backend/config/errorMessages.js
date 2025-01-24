a95ce99c4f5703cfc5c5e59475e9ee31474f0a92
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-01-24 15:43:37 -0300

    add the errorMessages object

---
+++ backend/config/errorMessages.js
@@ -0,0 +1,8 @@
+const errorMessages = {
+  INTERNAL_SERVER_ERROR: "Internal server error",
+  WITHOUT_BODY: "Body is required",
+  BOOK_NOT_EXISTS: "The book does not exist",
+  PUBLICATION_DATE_INVALID: "Publication date invalid",
+};
+
+module.exports = errorMessages;
