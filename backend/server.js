515a7cb5d59f4c62df252f69ab4786e7276eceb4
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-01-24 15:55:18 -0300

    refac the server-js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 3000;
-const bookRouter = require("./book/bookRoutes");
 const { errors } = require("celebrate");
+
 const swaggerUI = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
-const app = express();
-app.use(cors());
-app.use(express.json({ limit: "1mb" }));
+const bookRouter = require("./book/bookRoutes");
 
-app.get("/", (req, res) => {
-  res.json({ message: `Listening at port ${PORT}` });
-});
+const app = express();
 
 app.use("/book", bookRouter);
 
-app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
-
+app.use(cors());
+app.use(express.json({ limit: "1mb" }));
 app.use(errors());
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
