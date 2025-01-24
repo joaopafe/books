0e4438e5af45c15907ef5ef439ef0fc210166558
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-01-24 15:45:21 -0300

    fix the problem of error handling with the database cases

--- backend/book/BookController.js
+++ backend/book/BookController.js
@@ -1,20 +1,34 @@
 const BookRepository = require("../repository/BookRepository");
 
+const errorMessages = require("../config/errorMessages");
+
 class BookController {
   static async createTable() {
     await BookRepository.createTable();
   }
 
   static async listAll(req, res) {
-    const books = await BookRepository.listAll();
+    try {
+      const books = await BookRepository.listAll();
 
-    return res.status(200).json(books);
+      return res.status(200).json(books);
+    } catch {
+      return res
+        .status(500)
+        .json({ message: errorMessages.INTERNAL_SERVER_ERROR });
+    }
   }
 
   static async listById(req, res) {
-    const book = await BookRepository.listById(req.params.id);
+    try {
+      const book = await BookRepository.listById(req.params.id);
 
-    return res.status(200).json(book);
+      return res.status(200).json(book);
+    } catch {
+      return res
+        .status(500)
+        .json({ message: errorMessages.INTERNAL_SERVER_ERROR });
+    }
   }
 
   static async create(req, res) {
@@ -24,15 +38,21 @@ class BookController {
     const description = req.body.description;
     const image = req.body.image;
 
-    await BookRepository.create(
-      title,
-      author,
-      publicationDate,
-      description,
-      image
-    );
+    try {
+      await BookRepository.create(
+        title,
+        author,
+        publicationDate,
+        description,
+        image
+      );
 
-    return res.status(201).json({ message: "Book created succesfully" });
+      return res.status(201).json({ message: "Book created succesfully" });
+    } catch {
+      return res
+        .status(500)
+        .json({ message: errorMessages.INTERNAL_SERVER_ERROR });
+    }
   }
 
   static async update(req, res) {
@@ -43,24 +63,35 @@ class BookController {
     const description = req.body.description;
     const image = req.body.image;
 
-    await BookRepository.update(
-      id,
-      title,
-      author,
-      publicationDate,
-      description,
-      image
-    );
-
-    return res.status(200).json({ message: "Book updated succesfully" });
+    try {
+      await BookRepository.update(
+        id,
+        title,
+        author,
+        publicationDate,
+        description,
+        image
+      );
+      return res.status(200).json({ message: "Book updated succesfully" });
+    } catch {
+      return res
+        .status(500)
+        .json({ message: errorMessages.INTERNAL_SERVER_ERROR });
+    }
   }
 
   static async delete(req, res) {
     const id = req.params.id;
 
-    await BookRepository.delete(id);
+    try {
+      await BookRepository.delete(id);
 
-    return res.status(200).json({ message: "Book deleted succesfully" });
+      return res.status(200).json({ message: "Book deleted succesfully" });
+    } catch {
+      return res
+        .status(500)
+        .json({ message: errorMessages.INTERNAL_SERVER_ERROR });
+    }
   }
 }
 
