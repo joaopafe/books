aa4ea2bfadf5d65221fdf77d6ec11c4df638eb04
joaopafe <joao.ea2020@gmail.com>
2025-04-30 13:39:26 -0300

    fix the problem with the put and post routes

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,9 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json({ limit: "1mb" }));
 app.use("/book", bookRouter);
 
-app.use(express.json({ limit: "1mb" }));
 app.use(errors());
 app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
